574182f3565cf3bc2f7336d16b2dc9c25cb312f2
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-01 13:34:36 +0500

    Added Console implementation for Composite Pattern

---
+++ src/CompositePatternInConsole/Composite.cs
@@ -0,0 +1,44 @@
+﻿using CompositePatternInConsole.Abstracts;
+
+namespace CompositePatternInConsole;
+
+// The Composite class represents the complex components that may have children.
+// Usually, the Composite objects delegate the actual work to their children and
+// then "sum-up" the result.
+
+public class Composite : Component
+{
+    protected List<Component> _children = [];
+    // A composite object can add or remove other components (both simple or
+    // complex) to or from its child list.
+
+    public override void Add(Component component)
+    {
+        this._children.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        this._children.Remove(component);
+    }
+
+    // A composite object executes its primary logic in a particular way. It
+    // traverses recursively through all its children, collecting and summing their
+    // results. Since the composite's children pass these calls to their children
+    // and so forth, the whole object tree is traversed as a result.
+    public override string Operation()
+    {
+        int i = 0;
+        string result = "Branch(";
+        foreach (Component component in this._children)
+        {
+            result += component.Operation();
+            if (i != this._children.Count - 1)
+            {
+                result += "+";
+            }
+            i++;
+        }
+        return result + ")";
+    }
+}
